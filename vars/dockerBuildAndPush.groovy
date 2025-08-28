b89c46e62c1e158bfa81dcbff565035fb8230f6c
KonpalM <138207736+KonpalM@users.noreply.github.com>
2025-08-28 18:14:10 +0000

    Update dockerBuildAndPush.groovy

--- vars/dockerBuildAndPush.groovy
+++ vars/dockerBuildAndPush.groovy
@@ -1,9 +1,10 @@
 def call(Map args = [:]) {
-    def utils = new org.example.Utils()
+    // Initialize Utils with the current pipeline context
+    def utils = new org.example.Utils(this)
 
     // Required params
-    def image = args.get('image', null)
-    def tag   = args.get('tag', 'latest')
+    def image    = args.get('image', null)
+    def tag      = args.get('tag', 'latest')
     def registry = args.get('registry', 'docker.io')
 
     if (!image) {
@@ -11,10 +12,19 @@ def call(Map args = [:]) {
     }
 
     stage("Docker Build & Push") {
-        sh """
-            docker build -t ${registry}/${image}:${tag} .
-            docker push ${registry}/${image}:${tag}
-        """
+        if (isUnix()) {
+            // Linux/Mac agents
+            sh """
+                docker build -t ${registry}/${image}:${tag} .
+                docker push ${registry}/${image}:${tag}
+            """
+        } else {
+            // Windows agents
+            bat """
+                docker build -t ${registry}\\${image}:${tag} .
+                docker push ${registry}\\${image}:${tag}
+            """
+        }
 
         echo "Successfully built and pushed ${registry}/${image}:${tag}"
     }
