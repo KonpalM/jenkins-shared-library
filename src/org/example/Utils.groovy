3aaf24daa95cd2f9eef9bd804c41c5da45b100ce
KonpalM <138207736+KonpalM@users.noreply.github.com>
2025-08-28 19:00:46 +0100

    Update Utils.groovy

--- src/org/example/Utils.groovy
+++ src/org/example/Utils.groovy
@@ -1,13 +1,13 @@
-package org.example
+package org.example   // defines the package name (like a namespace)
 
 class Utils {
-    def steps
+    def steps   // this will hold Jenkins "steps" like echo, sh, etc.
 
-    Utils() {
-        this.steps = org.jenkinsci.plugins.workflow.cps.CpsScript.current()
+    Utils(steps) {   // constructor → called when we create new Utils(this)
+        this.steps = steps
     }
 
-    def notify(String message) {
-        steps.echo "Notification: ${message}"
+    def notify(String message) {   // function
+        steps.echo "🔔 Notification: ${message}"
     }
 }
